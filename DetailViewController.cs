47c426cd3a2bffb3c4d1a2db3980f74a16283339
JordanY1 <Jordany1@learn.cssd.ab.ca>
2017-10-19 13:34:17 -0600

    Update

---
+++ DetailViewController.cs
@@ -0,0 +1,40 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BMICalculator
+{
+    public partial class DetailViewController : UIViewController
+    {
+        public float bmi = 21.5f;
+
+        public DetailViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            EvaluateBMI();
+        }
+
+        void EvaluateBMI()
+        {
+            string result = String.Empty;
+
+            if (bmi <= 16)
+                result = "Very Low";
+            else if (bmi < 18.5)
+                result = "Low";
+            else if (bmi < 25)
+                result = "Normal";
+            else if (bmi < 30)
+                result = "high";
+            else
+                result = "Too High";
+
+            resultLabel.Text = result;
+        }
+    }
+}
--- ViewController.cs
+++ ViewController.cs
@@ -6,7 +6,8 @@ namespace BMICalculator
 {
     public partial class ViewController : UIViewController
     {
-        protected ViewController(IntPtr handle) : base(handle)
+        float bmi = 21.5f;
+        public ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
         }
@@ -15,12 +16,34 @@ namespace BMICalculator
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            calculateButton.TouchUpInside+= CalculateButton_TouchUpInside;
+        }
+
+        void CalculateButton_TouchUpInside(object sender, EventArgs e)
+        {
+            var height = float.Parse(heightTextField.Text);
+            var weight = float.Parse(weightTextField.Text);
+
+            bmi = (weight / (height * height));
+
+            bmiLabel.Text = bmi.ToString();
+
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, Foundation.NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+
+            var detailViewController = segue.DestinationViewController as DetailViewController;
+
+            detailViewController.bmi = this.bmi;
         }
     }
 }
